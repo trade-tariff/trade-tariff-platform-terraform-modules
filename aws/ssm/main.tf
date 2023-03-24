39e33177e07f5c2edbc5d6040985644c879dffed
Amber Cronin <amber@amber.vision>
2023-03-24 09:35:44 +0000

    BAU: for_each needs each.value

--- aws/ssm/main.tf
+++ aws/ssm/main.tf
@@ -11,18 +11,16 @@ resource "aws_kms_key" "this" {
   count               = var.create_kms_key == true ? 1 : 0
   key_usage           = "ENCRYPT_DECRYPT"
   enable_key_rotation = true
-
-  tags = local.tags
+  tags                = local.tags
 }
 
 resource "aws_ssm_parameter" "this" {
-  for_each = var.parameters
-
-  name   = parameter.value.name
-  value  = parameter.value.value
-  tier   = try(parameter.value.tier, "Standard")
-  type   = "SecureString"
-  key_id = var.create_kms_key == true ? aws_kms_key.this.key_id : null
-
-  tags = local.tags
+  for_each  = var.parameters
+  data_type = "text"
+  name      = each.value.name
+  value     = each.value.value
+  tier      = try(each.value.tier, "Standard")
+  type      = "SecureString"
+  key_id    = var.create_kms_key == true ? aws_kms_key.this.key_id : null
+  tags      = local.tags
 }
