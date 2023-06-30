e007a735e12c0dfedc6375d7e3b64efc89131b07
Bun Camara <bun.camara@transformuk.com>
2023-06-30 13:29:02 +0100

    remove string

--- aws/ecs-service/outputs.tf
+++ aws/ecs-service/outputs.tf
@@ -1,4 +1,4 @@
 output "task_execution_role_arn" {
   description = "Task execution role arn"
-  value       = "aws_iam_role.ecs_tasks_execution_role.arn"
+  value       = aws_iam_role.ecs_tasks_execution_role.arn
 }
