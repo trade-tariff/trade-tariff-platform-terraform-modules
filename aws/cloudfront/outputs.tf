7b906e062a5d8552a85865d9c72b9c6570fea919
Amber Cronin <amber@amber.vision>
2023-07-31 11:24:23 +0100

    BAU: Fix output indexing

--- aws/cloudfront/outputs.tf
+++ aws/cloudfront/outputs.tf
@@ -7,9 +7,9 @@ output "aws_cloudfront_distribution_arn" {
 }
 
 output "aws_cloudfront_distribution_domain_name" {
-  value = aws_cloudfront_distribution.this.domain_name
+  value = aws_cloudfront_distribution.this[0].domain_name
 }
 
 output "aws_cloudfront_distribution_hosted_zone_id" {
-  value = aws_cloudfront_distribution.this.hosted_zone_id
+  value = aws_cloudfront_distribution.this[0].hosted_zone_id
 }
