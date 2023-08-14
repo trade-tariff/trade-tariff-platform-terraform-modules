e73114e50192fa7d3d6d112b43797367931474f1
Amber Cronin <amber@amber.vision>
2023-08-14 14:31:27 +0100

    BAU: Make load balancer block optional

--- aws/ecs-service/main.tf
+++ aws/ecs-service/main.tf
@@ -8,10 +8,13 @@ resource "aws_ecs_service" "this" {
 
   enable_execute_command = var.enable_ecs_exec
 
-  load_balancer {
-    container_name   = var.service_name
-    container_port   = var.container_port
-    target_group_arn = var.target_group_arn
+  dynamic "load_balancer" {
+    for_each = var.target_group_arn != null ? [1] : []
+    content {
+      container_name   = var.service_name
+      container_port   = var.container_port
+      target_group_arn = var.target_group_arn
+    }
   }
 
   network_configuration {
