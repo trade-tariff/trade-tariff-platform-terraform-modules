8d368e15cf666d00d3bd09dc7a78ccce2d95848f
Amber Cronin <amber@amber.vision>
2025-06-09 14:46:15 +0100

    Remove github modules
    They are no longer required in this repo.

--- github/repos/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "repo_node_id" {
-  value = github_repository.repo.node_id
-}
--- github/users/main.tf
+++
@@ -1,7 +0,0 @@
-resource "github_team_membership" "this" {
-  count = length(var.teams)
-
-  team_id  = var.teams[count.index]
-  username = var.username
-  role     = var.is_maintainer ? "maintainer" : "member"
-}
